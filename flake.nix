7a7524d739e733b3a129c6c2305869cab7d07bbb
LiAlH4 <lialh4qwq@outlook.com>
2026-04-21 18:16:44 +0800

    reflactor flake.nix
    reflactor: using `flake-parts` instead of `flake-utils` due to the later
    is unrecommended yet
    feature: add `linquebot_rs` (without `lm` module) package and
    `linquebot_rs_lm` (with `lm` module) package, and make `default` package
    a alias to the former
    fix: fix build failure under devshell due to missing native libs
    chores: make lots of things more precise and delarative, like shrink the
    `with` to minimal scope

--- flake.nix
+++ flake.nix
@@ -17,132 +17,136 @@
   };
 
   inputs = {
+    systems.url = "github:nix-systems/default";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    crane.url = "github:ipetkov/crane";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    crane.url = "github:ipetkov/crane";
   };
 
   outputs =
-    {
-      nixpkgs,
-      flake-utils,
-      rust-overlay,
-      crane,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        overlays = [
-          (import rust-overlay)
-        ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        craneLib = (crane.mkLib pkgs).overrideToolchain (
-          p: p.rust-bin.selectLatestNightlyWith (toolchain: toolchain.minimal)
-        );
-
-      in
-      rec {
-        devShells.default =
-          with pkgs;
-          mkShell {
-            buildInputs = [
-              cmake
-              openssl
-              graphviz
-              (rust-bin.selectLatestNightlyWith (
-                toolchain:
-                toolchain.default.override {
-                  extensions = [
-                    "rust-src"
-                    "clippy-preview"
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } (
+      { lib, ... }:
+      {
+        systems = import inputs.systems;
+        perSystem =
+          { self', system, ... }:
+          let
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ inputs.rust-overlay.overlays.default ];
+            };
+          in
+          {
+            _module.args.pkgs = pkgs;
+            devShells.default = pkgs.mkShell {
+              LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+              BINDGEN_EXTRA_CLANG_ARGS = "-isystem ${pkgs.libclang.lib}/lib/clang/${lib.versions.major (lib.getVersion pkgs.libclang)}/include ${lib.fileContents "${pkgs.stdenv.cc}/nix-support/libc-cflags"}";
+              buildInputs = [
+                pkgs.pkg-config
+                pkgs.cmake
+                pkgs.graphviz
+                (pkgs.rust-bin.selectLatestNightlyWith (
+                  toolchain:
+                  toolchain.default.override {
+                    extensions = [
+                      "rust-src"
+                      "clippy-preview"
+                    ];
+                  }
+                ))
+              ]
+              ++ lib.optional pkgs.stdenvNoCC.hostPlatform.isLinux pkgs.openssl;
+            };
+            packages = {
+              default = self'.packages.linquebot_rs;
+              linquebot_rs_lm = self'.packages.linquebot_rs.override { lm = true; };
+              linquebot_rs =
+                let
+                  craneLib = (inputs.crane.mkLib pkgs).overrideToolchain (
+                    p: p.rust-bin.selectLatestNightlyWith (toolchain: toolchain.minimal)
+                  );
+                in
+                lib.makeOverridable (
+                  {
+                    lm ? false,
+                  }:
+                  craneLib.buildPackage {
+                    src =
+                      let
+                        jsonFilter = path: _type: builtins.match ".*json$" path != null;
+                        jsonOrCargo = path: type: (jsonFilter path type) || (craneLib.filterCargoSources path type);
+                      in
+                      lib.cleanSourceWith {
+                        src = ./.;
+                        filter = jsonOrCargo;
+                        name = "source";
+                      };
+                    buildInputs = [
+                      pkgs.pkg-config
+                      pkgs.cmake
+                    ]
+                    ++ lib.optional pkgs.stdenvNoCC.hostPlatform.isLinux pkgs.openssl;
+                    nativeBuildInputs = [ pkgs.makeWrapper ];
+                    CI = "true";
+                    stdenv = p: p.clangStdenv;
+                    LIBCLANG_PATH = "${pkgs.libclang.lib}/lib";
+                    cargoExtraArgs = lib.optionalString lm "--features lm";
+                    postInstall = ''
+                      wrapProgram $out/bin/linquebot_rs --prefix PATH : ${lib.makeBinPath [ pkgs.graphviz ]}
+                    '';
+                    meta.mainProgram = "linquebot_rs";
+                  }
+                ) { };
+              dockerSupports = pkgs.stdenvNoCC.mkDerivation {
+                name = "linquebot_rs-docker-supports";
+                dontUnpack = true;
+                buildInputs = [
+                  pkgs.fontconfig
+                ];
+                installPhase =
+                  let
+                    fonts-conf = pkgs.makeFontsConf {
+                      fontDirectories = with pkgs; [
+                        twemoji-color-font
+                        noto-fonts-cjk-sans
+                        noto-fonts
+                      ];
+                    };
+                  in
+                  ''
+                    runHook preInstall
+                    mkdir -p $out/etc/fonts/conf.d $out/var
+                    mkdir -m 1777 $out/tmp
+                    cp ${fonts-conf} $out/etc/fonts/conf.d/99-nix.conf
+                    runHook postInstall
+                  '';
+              };
+              dockerImage = pkgs.dockerTools.buildLayeredImage {
+                name = "ghcr.io/lhcfl/linquebot_rs";
+                tag = "latest";
+                contents = [
+                  # coreutils
+                  pkgs.dockerTools.caCertificates
+                  pkgs.dockerTools.usrBinEnv
+                  # dockerTools.binSh
+                  # strace
+                  self'.packages.dockerSupports
+                ];
+                config = {
+                  Cmd = [
+                    "${lib.meta.getExe self'.packages.default}"
                   ];
-                }
-              ))
-            ];
-          };
-        packages.default =
-          with pkgs;
-          let
-            jsonFilter = path: _type: builtins.match ".*json$" path != null;
-            jsonOrCargo = path: type: (jsonFilter path type) || (craneLib.filterCargoSources path type);
-          in
-          craneLib.buildPackage {
-            src = lib.cleanSourceWith {
-              src = ./.;
-              filter = jsonOrCargo;
-              name = "source";
+                  WorkingDir = "/app";
+                  StopSignal = "SIGINT";
+                };
+                created = "now";
+              };
             };
-            # Add extra inputs here or any other derivation settings
-            # doCheck = true;
-            buildInputs = [
-              pkg-config
-              cmake
-            ]
-            ++ lib.optionals stdenvNoCC.hostPlatform.isLinux [
-              openssl
-            ];
-            nativeBuildInputs = [ makeWrapper ];
-            CI = "true";
-            stdenv = p: p.clangStdenv;
-            LIBCLANG_PATH = "${libclang.lib}/lib";
-            postInstall = ''
-              wrapProgram $out/bin/linquebot_rs --prefix PATH : ${lib.makeBinPath [ graphviz ]}
-            '';
-            meta.mainProgram = "linquebot_rs";
-          };
-        packages.dockerSupports =
-          with pkgs;
-          let
-            fonts-conf = makeFontsConf {
-              fontDirectories = [
-                twemoji-color-font
-                noto-fonts-cjk-sans
-                noto-fonts
-              ];
-            };
-          in
-          stdenvNoCC.mkDerivation {
-            name = "linquebot_rs-docker-supports";
-            dontUnpack = true;
-            buildInputs = [
-              fontconfig
-            ];
-            installPhase = ''
-              runHook preInstall
-              mkdir -p $out/etc/fonts/conf.d $out/var
-              mkdir -m 1777 $out/tmp
-              cp ${fonts-conf} $out/etc/fonts/conf.d/99-nix.conf
-              runHook postInstall
-            '';
-          };
-        packages.dockerImage =
-          with pkgs;
-          dockerTools.buildLayeredImage {
-            name = "ghcr.io/lhcfl/linquebot_rs";
-            tag = "latest";
-            contents = [
-              # coreutils
-              dockerTools.caCertificates
-              dockerTools.usrBinEnv
-              # dockerTools.binSh
-              # strace
-              packages.dockerSupports
-            ];
-            config = {
-              Cmd = [
-                "${lib.meta.getExe packages.default}"
-              ];
-              WorkingDir = "/app";
-              StopSignal = "SIGINT";
-            };
-            created = "now";
           };
       }
     );
